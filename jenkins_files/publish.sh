918d1d9615bf42423a4e39c043d52d1c937d505b
moccoh <mlac.gitacc@gmail.com>
2020-04-06 10:16:42 +0300

    east

--- jenkins_files/publish.sh
+++ jenkins_files/publish.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 echo "-------------BUILD and publish to ECR-------------"
-docker tag myserver:latest 384842694817.dkr.ecr.us-west-2.amazonaws.com/myserver:latest
-docker push 384842694817.dkr.ecr.us-west-2.amazonaws.com/myserver:latest
+docker tag myserver:latest 384842694817.dkr.ecr.us-east-2.amazonaws.com/myserver:latest
+docker push 384842694817.dkr.ecr.us-east-2.amazonaws.com/myserver:latest
